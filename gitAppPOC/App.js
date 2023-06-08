2fdb68c59a34b7ea27bb2505a4793c2c30de8df5
MrLuigiG <luigi@honeygrid.ai>
2023-06-08 16:47:32 -0600

    added theme

--- gitAppPOC/App.js
+++ gitAppPOC/App.js
@@ -1,44 +1,11 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
-
-const CreativeComponent = () => {
-  return (
-    <TouchableOpacity style={styles.creativeContainer}>
-      <Text style={styles.creativeText}>Click Me!</Text>
-    </TouchableOpacity>
-  );
-};
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import ThemedPage from './ThemedPage';
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const [inputText, setInputText] = useState('');
-
-  const incrementCounter = () => {
-    setCounter(counter + 1);
-  };
-
-  const decrementCounter = () => {
-    setCounter(counter - 1);
-  };
-
-  const handleInputChange = (text) => {
-    setInputText(text);
-  };
-
   return (
     <View style={styles.container}>
-      <Text style={styles.counterText}>Counter: {counter}</Text>
-      <View style={styles.buttonContainer}>
-        <Button title="Increment" onPress={incrementCounter} />
-        <Button title="Decrement" onPress={decrementCounter} />
-      </View>
-      <TextInput
-        style={styles.input}
-        onChangeText={handleInputChange}
-        value={inputText}
-        placeholder="Enter text"
-      />
-      <CreativeComponent />
+      <ThemedPage />
     </View>
   );
 };
@@ -48,33 +15,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  counterText: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '80%',
-    marginBottom: 20,
-  },
-  input: {
-    height: 40,
-    width: '80%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingHorizontal: 10,
-  },
-  creativeContainer: {
-    backgroundColor: 'lightblue',
-    padding: 10,
-    borderRadius: 5,
-  },
-  creativeText: {
-    fontSize: 18,
-    fontWeight: 'bold',
   },
 });
 
