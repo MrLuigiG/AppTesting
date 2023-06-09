459a61fa8abee2e9745af09125eb12118111697a
MrLuigiG <luigi@honeygrid.ai>
2023-06-09 11:31:39 -0600

    added theme

--- gitAppPOC/App.js
+++ gitAppPOC/App.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
 import ThemedPage from './ThemedPage';
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <ThemedPage />
-    </View>
-  );
+  return <ThemedPage />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default App;
